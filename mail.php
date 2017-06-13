6e705bc40bac958bb7eff2c9f4213f9d8814f5db
vdesai <amitbidwai.26@gmail.com>
2017-06-13 15:31:00 +0530

    Added Capcha and fix mail page

--- mail.php
+++ mail.php
@@ -58,26 +58,25 @@
                         </a>
                         <nav>
                             <ul class="cd-primary-nav">
-                                <li class="wrap"><a href="#about" class="btn-7 scroll"><span>ABOUT</span></a></li>
-                                <li class="wrap"><a href="#services" class="btn-7 scroll"><span>SERVICES</span></a></li>
-                                <li class="wrap"><a href="#portfolio" class="btn-7 scroll"><span>PORTFOLIO</span></a></li>
-                                <!-- <li class="wrap"><a href="#team" class="btn-7 scroll"><span>TEAM</span></a></li> -->
-                                <li class="wrap"><a href="#price" class="btn-7 scroll"><span>PRICE</span></a></li>
-                                <!-- <li class="wrap"><a href="#blog" class="btn-7 scroll"><span>BLOG</span></a></li> -->
-                                <li class="wrap"><a href="#contact" class="btn-7 scroll"><span>CONTACT</span></a></li>
+                                <li class="wrap"><a href="/index#about" class="btn-7  "><span>ABOUT</span></a></li>
+                                <li class="wrap"><a href="/index#services" class="btn-7  "><span>SERVICES</span></a></li>
+                                <li class="wrap"><a href="/index#portfolio" class="btn-7  "><span>PORTFOLIO</span></a></li>
+                                <!-- <li class="wrap"><a href="#team" class="btn-7  "><span>TEAM</span></a></li> -->
+                                <li class="wrap"><a href="/index#price" class="btn-7  "><span>PRICE</span></a></li>
+                                <!-- <li class="wrap"><a href="#blog" class="btn-7  "><span>BLOG</span></a></li> -->
+                                <li class="wrap"><a href="/index#contact" class="btn-7  "><span>CONTACT</span></a></li>
                             </ul>
-                        </nav>
                     </div>
                         <nav class="main-nav">
                         <ul>
-                            <li class="wrap"><a href="#home" class="btn-7 scroll"><span>HOME</span></a></li>
-                            <li class="wrap"><a href="#about" class="btn-7 scroll"><span>ABOUT</span></a></li>
-                            <li class="wrap"><a href="#services" class="btn-7 scroll"><span>SERVICES</span></a></li>
-                            <li class="wrap"><a href="#portfolio" class="btn-7 scroll"><span>PORTFOLIO</span></a></li>
+                            <li class="wrap"><a href="/index#home" class="btn-7"><span>HOME</span></a></li>
+                            <li class="wrap"><a href="/index#about" class="btn-7"><span>ABOUT</span></a></li>
+                            <li class="wrap"><a href="/index#services" class="btn-7"><span>SERVICES</span></a></li>
+                            <li class="wrap"><a href="/index#portfolio" class="btn-7"><span>PORTFOLIO</span></a></li>
                             <!-- <li class="wrap"><a href="#team" class="btn-7 scroll"><span>TEAM</span></a></li> -->
-                            <li class="wrap"><a href="#price" class="btn-7 scroll"><span>PRICE</span></a></li>
+                            <li class="wrap"><a href="/index#price" class="btn-7"><span>PRICE</span></a></li>
                             <!-- <li class="wrap"><a href="#blog" class="btn-7 scroll"><span>BLOG</span></a></li> -->
-                            <li class="wrap"><a href="#contact" class="btn-7 scroll"><span>CONTACT</span></a></li>
+                            <li class="wrap"><a href="/index#contact" class="btn-7"><span>CONTACT</span></a></li>
 
                         </ul>
                     </nav>
@@ -92,7 +91,53 @@
   <section class="page-content detail-page">
         <div class="container page-content-wrapper">
           <div class="col-sm-12">
-<?php 
+
+<?php
+//Checking For reCAPTCHA
+$captcha;
+if (isset($_POST['g-recaptcha-response'])) {
+    $captcha = $_POST['g-recaptcha-response'];
+}
+// Checking For correct reCAPTCHA
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=SECRETKEY&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+if (!$captcha || $response.success == false) {
+    echo "Your CAPTCHA response was wrong.";
+    exit ;
+} else {
+    // Checking For Blank Fields..
+    if ($_POST["vname"] == "" || $_POST["vemail"] == "" || $_POST["msg"] == "") {
+        echo "Fill All Fields..";
+    } else {
+        // Check if the "Sender's Email" input field is filled out
+        $email = $_POST['vemail'];
+        // Sanitize E-mail Address
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        // Validate E-mail Address
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if (!$email) {
+            echo "Invalid Sender's Email";
+        } else {
+            $to = 'amitbidwai.26@gmail.com';
+            $subject = 'Website Responce';
+            $message = $_POST['msg'];
+            $headers = 'From:' . $email . "\r\n";
+            // Sender's Email
+            // Message lines should not exceed 70 characters (PHP rule), so wrap it
+            $message = wordwrap($message, 70, "\r\n");
+            // Send Mail By PHP Mail Function
+            if (mail($to, $subject, $message, $headers)) {
+                echo "Your mail has been sent successfully!";
+            } else {
+                echo "Failed to send email, try again.";
+                exit ;
+            }
+        }
+    }
+}
+?>
+
+
+<!-- <?php 
 if(isset($_POST['submit'])){
     $to =    "contact@bidwai.in";
     $from = $_POST['email'];
@@ -118,7 +163,7 @@ if(isset($_POST['submit'])){
 echo "Did not entered mail module". "Name:". $name . "\n\nEmail : ". $email ."\n\nWrote the following:" . "\n\n" . $message . " \n\n Regards Team\n Bidwai Technologies";
 
     }
-?>
+?> -->
 <!-- Footer Start-->
 <?php include 'thank.php';
 ?>
@@ -146,13 +191,14 @@ echo "Did not entered mail module". "Name:". $name . "\n\nEmail : ". $email ."\n
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-footer footer-nav first-nav">
                     <h3>Additional Links</h3>
-                    <ul>
-                        <li><a href="#home" class="scroll">HOME</a></li>
-                        <li><a href="#about" class="scroll">ABOUT</a></li>
-                        <li><a href="#services" class="scroll">SERVICES</a></li>
-                        <li><a href="#portfolio" class="scroll">PORTFOLIO</a></li>
-                       <!--  <li><a href="#team" class="scroll">TEAM</a></li>
-                         <li><a href="#blog" class="scroll">BLOG</a></li>-->
+                   <ul>
+                        <li></li>
+                        <li><a href="/index#home" class=" ">HOME</a></li>
+                        <li><a href="/index#about" class=" ">ABOUT</a></li>
+                        <li><a href="/index#services" class=" ">SERVICES</a></li>
+                        <li><a href="/index#portfolio" class=" ">PORTFOLIO</a></li>
+                        <li><a href="/index#team" class=" ">TEAM</a></li>
+                        <li><a href="/index#blog" class=" ">BLOG</a></li>
                     </ul>
                 </div>
                 <div class="col-clearFix"></div>

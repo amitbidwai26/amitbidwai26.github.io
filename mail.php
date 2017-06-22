abf42b32acad553a6dfd4426a28c3bf88f46bde0
vdesai <amitbidwai.26@gmail.com>
2017-06-22 19:41:40 +0530

    Working comtact form and mail functionality

--- mail.php
+++ mail.php
@@ -117,10 +117,11 @@ if (!$captcha || $response.success == false) {
         if (!$email) {
             echo "Invalid Sender's Email";
         } else {
-            $to = 'amitbidwai.26@gmail.com';
+            $from = 'contact@bidwai.in';
+            $to = $email;
             $subject = 'Website Responce';
             $message = $_POST['msg'];
-            $headers = 'From:' . $email . "\r\n";
+            $headers = "From:" . $from;
             // Sender's Email
             // Message lines should not exceed 70 characters (PHP rule), so wrap it
             $message = wordwrap($message, 70, "\r\n");
@@ -137,33 +138,7 @@ if (!$captcha || $response.success == false) {
 ?>
 
 
-<!-- <?php 
-if(isset($_POST['submit'])){
-    $to =    "contact@bidwai.in";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $subject = "Form submission on website";
-    $subject2 = "Copy of your form submission on website";
-    $messagetosend = "Name:". $name . "\n\nEmail : ". $email ."\n\nWrote the following:" . "\n\n" . $message . " \n\n Regards Team\nBidwai Technologies";
-    $messagetocopy = "Name:". $name . "\n\nEmail : ". $email ."\n\nWrote the following:" . "\n\n" . $message . " \n\n Regards Team\nBidwai Technologies";
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    if(isset($_POST['email'])){
-            mail($to,$subject,$messagetosend,$headers);
-            mail($from,$subject2,$messagetocopy,$headers2);
-// mail($to1,$subject,$messagetosend,$headers); // sends a copy of the message to the sender
-            echo "Mail Sent Sucessfully";
-    }
-    echo "<center><br>Copy of information is sent via Mail. <br><br> Thank you " . $first_name . ", <br><br>we will contact you shortly.</center>";
-    }else{
-
-echo "Did not entered mail module". "Name:". $name . "\n\nEmail : ". $email ."\n\nWrote the following:" . "\n\n" . $message . " \n\n Regards Team\n Bidwai Technologies";
-
-    }
-?> -->
 <!-- Footer Start-->
 <?php include 'thank.php';
 ?>
